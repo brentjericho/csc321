4091ca8897ac6f51d33db0ecbee71b31c2857b53
bu <buyguangco1@toromail.csudh.edu>
2025-02-11 19:18:24 +0000

    Lab #4 first version

---
+++ buyguangco4.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class buyguangco4
+{
+        public static void main(String[] args)
+        {
+                
+                System.out.println("Pick a Beverage: Water(1), Coffee(2), Tea(3), Beer(4)");
+                Scanner scanner = new Scanner(System.in);
+		boolean isValid = false;
+		
+		while (isValid)
+		{	
+			int choice = scanner.nextInt();
+			
+			if(choice == 1)
+                	{
+                        	System.out.println("Water, coming your way!");
+				isValid = true;
+                	}
+                	else if(choice == 2)
+                	{
+                        	System.out.println("Coffee, coming your way!");
+				isValid = true;
+                	}
+			else if(choice == 3)
+			{
+				System.out.println("Tea, coming your way!");
+				isValid = true;
+			}
+			else if(choice == 4)
+                	{
+                        	System.out.println("Beer, coming your way!");
+				isValid = true;
+			}
+			else
+			{
+				System.out.println("Invalid option, please try again.");
+			}
+		}
+	}
+}
