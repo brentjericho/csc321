2b58c9846cd801389920d9a408a2f9c24cd5b784
bu <buyguangco1@toromail.csudh.edu>
2025-02-26 02:36:28 +0000

    Lab 7 Python file

---
+++ buyguangco7.py
@@ -0,0 +1,23 @@
+# Brent Jericho Uyguangco
+# CSC 321 Lab 7
+
+num = 9
+
+if num >= 9:
+    print("Correct, num is greater than or equal to 9")
+else:
+    print("Wrong!")
+
+dec = 6.9
+       
+if dec > 10.6:
+    print("Correct!")
+else:
+    print("Wrong!")      
+
+word = "Software"
+           
+if word == "Software":
+    print("Correct, the word you are looking for is Software")
+else:
+    print("Wrong!")      
