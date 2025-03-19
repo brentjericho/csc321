1147b377dbdfae79a77b977973a9859277633f80
bu <buyguangco1@toromail.csudh.edu>
2025-03-19 01:19:37 +0000

    Lab 10 Java program

---
+++ buyguangco10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class buyguangco10
+{
+        public static void main(String[] args)
+        {
+                double radius, height;
+
+                Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter radius: ");
+		radius = scanner.nextDouble();
+		System.out.println("Enter height: ");                                                                                   height = scanner.nextDouble();
+
+		double area = area(radius, height);
+
+		System.out.println("The volume of the cylinder is: " + area);
+       	}
+
+	public static double area(double radius, double height)
+	{
+		double area = (Math.PI) * (Math.pow(radius, 2)) * height;
+		return area;
+
+	}
+}
